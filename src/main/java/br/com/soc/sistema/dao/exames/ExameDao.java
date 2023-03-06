0299c134cc855024953462b392231588ddf07cb3
Leonardo <leonardo.pereira204@etec.sp.gov.br>
2023-03-05 21:07:05 -0300

    crud exames, ok

--- src/main/java/br/com/soc/sistema/dao/exames/ExameDao.java
+++ src/main/java/br/com/soc/sistema/dao/exames/ExameDao.java
@@ -12,97 +12,91 @@ import br.com.soc.sistema.dao.Dao;
 import br.com.soc.sistema.vo.ExameVo;
 
 public class ExameDao extends Dao {
-	
-	public void insertExame(ExameVo exameVo){
+
+	public void insertExame(ExameVo exameVo) {
 		StringBuilder query = new StringBuilder("INSERT INTO exame (nm_exame) values (?)");
-		try(
-			Connection con = getConexao();
-			PreparedStatement  ps = con.prepareStatement(query.toString())){
-			
-			int i=1;
+		try (Connection con = getConexao(); PreparedStatement ps = con.prepareStatement(query.toString())) {
+
+			int i = 1;
 			ps.setString(i++, exameVo.getNome());
 			ps.executeUpdate();
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public List<ExameVo> findAllExames(){
+
+	public List<ExameVo> findAllExames() {
 		StringBuilder query = new StringBuilder("SELECT rowid id, nm_exame nome FROM exame");
-		try(
-			Connection con = getConexao();
-			PreparedStatement  ps = con.prepareStatement(query.toString());
-			ResultSet rs = ps.executeQuery()){
-			
-			ExameVo vo =  null;
+		try (Connection con = getConexao();
+				PreparedStatement ps = con.prepareStatement(query.toString());
+				ResultSet rs = ps.executeQuery()) {
+
+			ExameVo vo = null;
 			List<ExameVo> exames = new ArrayList<>();
 			while (rs.next()) {
 				vo = new ExameVo();
 				vo.setRowid(rs.getString("id"));
-				vo.setNome(rs.getString("nome"));	
-				
+				vo.setNome(rs.getString("nome"));
+
 				exames.add(vo);
 			}
 			return exames;
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return Collections.emptyList();
 	}
-	
-	public List<ExameVo> findAllByNome(String nome){
+
+	public List<ExameVo> findAllByNome(String nome) {
 		StringBuilder query = new StringBuilder("SELECT rowid id, nm_exame nome FROM exame ")
-								.append("WHERE lower(nm_exame) like lower(?)");
-		
-		try(Connection con = getConexao();
-			PreparedStatement ps = con.prepareStatement(query.toString())){
+				.append("WHERE lower(nm_exame) like lower(?)");
+
+		try (Connection con = getConexao(); PreparedStatement ps = con.prepareStatement(query.toString())) {
 			int i = 1;
-			
-			ps.setString(i, "%"+nome+"%");
-			
-			try(ResultSet rs = ps.executeQuery()){
-				ExameVo vo =  null;
+
+			ps.setString(i, "%" + nome + "%");
+
+			try (ResultSet rs = ps.executeQuery()) {
+				ExameVo vo = null;
 				List<ExameVo> exames = new ArrayList<>();
-				
+
 				while (rs.next()) {
 					vo = new ExameVo();
 					vo.setRowid(rs.getString("id"));
-					vo.setNome(rs.getString("nome"));	
-					
+					vo.setNome(rs.getString("nome"));
+
 					exames.add(vo);
 				}
 				return exames;
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}		
+		}
 		return Collections.emptyList();
 	}
-	
-	public ExameVo findByCodigo(Integer codigo){
-		StringBuilder query = new StringBuilder("SELECT rowid id, nm_exame nome FROM exame ")
-								.append("WHERE rowid = ?");
-		
-		try(Connection con = getConexao();
-			PreparedStatement ps = con.prepareStatement(query.toString())){
+
+	public ExameVo findByCodigo(Integer codigo) {
+		StringBuilder query = new StringBuilder("SELECT rowid id, nm_exame nome FROM exame ").append("WHERE rowid = ?");
+
+		try (Connection con = getConexao(); PreparedStatement ps = con.prepareStatement(query.toString())) {
 			int i = 1;
-			
+
 			ps.setInt(i, codigo);
-			
-			try(ResultSet rs = ps.executeQuery()){
-				ExameVo vo =  null;
-				
+
+			try (ResultSet rs = ps.executeQuery()) {
+				ExameVo vo = null;
+
 				while (rs.next()) {
 					vo = new ExameVo();
 					vo.setRowid(rs.getString("id"));
-					vo.setNome(rs.getString("nome"));	
+					vo.setNome(rs.getString("nome"));
 				}
 				return vo;
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}		
+		}
 		return null;
 	}
 }
